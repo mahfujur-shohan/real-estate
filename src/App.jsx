ef9835ba6154dd90c193d096f01bb37b3bb4e7e8
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2024-10-19 00:49:52 +0600

    all the section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import AboutSection from "./components/AboutSection";
+import AvailableSection from "./components/AvailableSection";
+import ContactSection from "./components/ContactSection";
+import CtaSection from "./components/CtaSection";
+import FeatureSection from "./components/FeatureSection";
+import Footer from "./components/Footer";
+import MainSection from "./components/MainSection";
+import Navbar from "./components/Navbar";
+import QuestionSection from "./components/QuestionSection";
+import TeamSection from "./components/TeamSection";
+
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <Navbar />
+      <MainSection />
+      <FeatureSection />
+      <AvailableSection />
+      <AboutSection />
+      <CtaSection />
+      <QuestionSection />
+      <TeamSection />
+      <ContactSection />
+      <Footer />
     </>
   );
 }
